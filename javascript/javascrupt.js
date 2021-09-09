d189355cde7b1432b14514f175e934872367c1b3
Josue Tapia <josue.tapia@live.com.ar>
2021-09-09 09:39:56 -0300

    blog centrado

--- javascript/javascrupt.js
+++
@@ -1,7 +0,0 @@
-let burgerAnimate = document.getElementById('nav-icon3')
-let navhidde = document.getElementById('navhidde')
-
-burgerAnimate.addEventListener('click', () => {
-  burgerAnimate.classList.toggle('open')
-  navhidde.classList.toggle('navhidde')
-})
